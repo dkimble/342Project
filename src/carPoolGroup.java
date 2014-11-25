c8590aa3bedb4eaa9008e8207a57d2310168fbb0
Matt <Matt@Matt-HP>
2014-11-25 13:43:40 -0600

    Add comments

--- src/carPoolGroup.java
+++ src/carPoolGroup.java
@@ -1,16 +1,45 @@
 import java.util.LinkedList;
 
 
+/**
+ * Controls the functionality of a carpool group
+ *
+ */
 public class carPoolGroup implements Observer {
-	private static int count = 1; //used to generate group id's
+	/**
+	 * used to generate group id's
+	 */
+	private static int count = 1; 
+	
+	/**
+	 * The identifying id for the group
+	 */
 	private int groupID;	
+	
+	/**
+	 * The greatest number of riders who can be in the carpool
+	 */
 	private int maxCapacity;
+	
+	/**
+	 * The current number of riders in the carpool
+	 */
 	private int curCapacity;
 	
-	// Amount of time to leave before schedules arrival time
+	/**
+	 * Amount of delay time from received events
+	 */
 	private int delayTime;
+
+	/**
+	 * A linked list of all members of the carpool
+	 */
 	private LinkedList<person> riderList;
 	
+	/**
+	 * Creates a new carpool group with the given capacity
+	 * @param maxCapacity The greatest number of members the carpool can have
+	 */
 	public carPoolGroup(int maxCapacity){
 		this.groupID = carPoolGroup.count;
 		carPoolGroup.count++;
@@ -43,6 +72,11 @@ public class carPoolGroup implements Observer {
 		return riderList;
 	}
 	
+	/**
+	 * Adds a person to the carpool if there is space
+	 * @param newPerson The person to be added
+	 * @return true if person was added successfully, false otherwise
+	 */
 	public boolean addPerson(person newPerson){
 		if(this.curCapacity < this.maxCapacity){
 			this.riderList.add(newPerson);
@@ -56,42 +90,58 @@ public class carPoolGroup implements Observer {
 		
 	}
 	
-	
-	
 	public void update(CarPoolEvent e)
 	{
 		delayTime += e.getTimeDelay();
 	}
 	
+	/**
+	 * Rotates the driver of the car so all people get an equal share of driving
+	 */
 	public void updateSchedule()
 	{
 		//updates the driving schedule
 		delayTime = 0;
 		
+		// Move the first person on the list to the last position
 		person temp = riderList.remove(0);
 		riderList.addLast(temp);
 	}
 	
+	/**
+	 * Displays the daily schedule for the a group
+	 * Includes who is driving and how early that driver should leave to arrive on time
+	 */
 	public void displaySchedule()
 	{
 			
 			int maxDist = maxDistance(riderList);
 			int driverDist = riderList.getFirst().getDistFromSchool();
 			
-			int travelTime = calculateTravelTime(maxDist) + calculateTravelTime(maxDist-driverDist) + delayTime;
+			// Driver should drive to the farthest person away then turn back towards school
+			int travelTime = calculateTravelTime(maxDist-driverDist) + calculateTravelTime(maxDist) + delayTime;
 			
+			// Print out the driver and his travel time
 			System.out.print("Driver: " + riderList.getFirst().getName());
 			System.out.println("  Travel Time: " + travelTime + " Minutes");
+			
+			// Prints out the riders in the carpool
 			for(int i = 1; i<riderList.size(); i++)
 			{
 			System.out.println("Rider " + i + ": " + riderList.get(i).getName());
 			}
 	}
 	
+	/**
+	 * Finds how far the longest distance from any rider to school is
+	 * @param p The linked list of passengers in the car pool
+	 * @return The farthest any of the people live from school
+	 */
 	private static int maxDistance(LinkedList<person> p)
 	{
 		int max = 0;
 		
+		// Search through list for largest distance
 		for(int i = 0; i<p.size(); i++)
 		{
 			if(p.get(i).getDistFromSchool() > max)
@@ -101,11 +151,19 @@ public class carPoolGroup implements Observer {
 		return max;
 	}
 	
+	/**
+	 * Calculates how long a drive of a certain distance should take
+	 * @param distance The distance that needs to be traveled
+	 * @return The amount of time in minutes the drive should take
+	 */
 	private static int calculateTravelTime(int distance)
 	{
+		// Assume an average driving speed of 20 MPH
 		int avgMPH = 20;
+		
 		double timeInHours = (double)distance / avgMPH;
 		int timeInMinutes = (int)(timeInHours * 60);
+		
 		return timeInMinutes;
 	}
 }
